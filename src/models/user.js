479f05d036013baa8028d90f785ff81c2565dc94
Mina Emad <menamanos@gmail.com>
2024-09-11 01:24:46 +0300

    Made email unique

--- src/models/user.js
+++ src/models/user.js
@@ -3,24 +3,25 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const schema = new Schema({
-	username: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	friends: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-		},
-	],
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
 });
 
 const User = model("User", schema);
