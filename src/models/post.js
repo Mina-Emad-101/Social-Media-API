c27926f628bd4e68341e8688ebad438a796dc86e
Mina Emad <menamanos@gmail.com>
2024-09-11 01:35:51 +0300

    posts author_id instead of author

--- src/models/post.js
+++ src/models/post.js
@@ -3,22 +3,22 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const schema = new Schema({
-	author: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "User",
-		required: true,
-	},
-	text: {
-		type: String,
-		required: true,
-	},
-	attachment: {
-		type: String,
-	},
-	likes: {
-		type: Number,
-		required: true,
-	},
+  author_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  attachment: {
+    type: String,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
 });
 
 const Post = model("Post", schema);
