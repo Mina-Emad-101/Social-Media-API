3bcaea7fd3126045cfbab35696a3a960fef612eb
Mina Emad <menamanos@gmail.com>
2024-09-10 23:45:44 +0300

    removed unnecessary dependency

--- src/validationSchemas/users.js
+++ src/validationSchemas/users.js
@@ -2,69 +2,69 @@ import { body } from "express-validator";
 import { passwordConfirmation } from "../utils/customValidators.js";
 
 export const createSchema = {
-	username: {
-		notEmpty: { errorMessage: "Username can't be empty" },
-		isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
-	},
-	email: {
-		notEmpty: { errorMessage: "Email can't be empty" },
-		isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
-	},
-	password: {
-		notEmpty: { errorMessage: "Password can't be empty" },
-		isStrongPassword: { errorMessage: "Password has to be strong" },
-	},
-	password_confirm: {
-		notEmpty: { errorMessage: "Password Confirmation can't be empty" },
-		custom: {
-			options: passwordConfirmation,
-			errorMessage: "Password and Password Confirmation not equal",
-		},
-	},
+  username: {
+    notEmpty: { errorMessage: "Username can't be empty" },
+    isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
+  },
+  email: {
+    notEmpty: { errorMessage: "Email can't be empty" },
+    isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
+  },
+  password: {
+    notEmpty: { errorMessage: "Password can't be empty" },
+    isStrongPassword: { errorMessage: "Password has to be strong" },
+  },
+  password_confirm: {
+    notEmpty: { errorMessage: "Password Confirmation can't be empty" },
+    custom: {
+      options: passwordConfirmation,
+      errorMessage: "Password and Password Confirmation not equal",
+    },
+  },
 };
 
 export const putSchema = {
-	username: {
-		notEmpty: { errorMessage: "Username can't be empty" },
-		isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
-	},
-	email: {
-		notEmpty: { errorMessage: "Email can't be empty" },
-		isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
-	},
-	password: {
-		notEmpty: { errorMessage: "Password can't be empty" },
-		isStrongPassword: { errorMessage: "Password has to be strong" },
-	},
-	password_confirm: {
-		notEmpty: { errorMessage: "Password Confirmation can't be empty" },
-		custom: {
-			options: passwordConfirmation,
-			errorMessage: "Password and Password Confirmation not equal",
-		},
-	},
+  username: {
+    notEmpty: { errorMessage: "Username can't be empty" },
+    isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
+  },
+  email: {
+    notEmpty: { errorMessage: "Email can't be empty" },
+    isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
+  },
+  password: {
+    notEmpty: { errorMessage: "Password can't be empty" },
+    isStrongPassword: { errorMessage: "Password has to be strong" },
+  },
+  password_confirm: {
+    notEmpty: { errorMessage: "Password Confirmation can't be empty" },
+    custom: {
+      options: passwordConfirmation,
+      errorMessage: "Password and Password Confirmation not equal",
+    },
+  },
 };
 
 export const patchSchema = {
-	username: {
-		optional: true,
-		isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
-	},
-	email: {
-		optional: true,
-		isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
-	},
-	password: {
-		if: body("password_confirm").notEmpty(),
-		notEmpty: { errorMessage: "Password can't be empty" },
-		isStrongPassword: { errorMessage: "Password has to be strong" },
-	},
-	password_confirm: {
-		if: body("password").notEmpty(),
-		notEmpty: { errorMessage: "Password Confirmation can't be empty" },
-		custom: {
-			options: passwordConfirmation,
-			errorMessage: "Password and Password Confirmation not equal",
-		},
-	},
+  username: {
+    optional: true,
+    isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
+  },
+  email: {
+    optional: true,
+    isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
+  },
+  password: {
+    if: (_, { req }) => req.body.password_confirm,
+    notEmpty: { errorMessage: "Password can't be empty" },
+    isStrongPassword: { errorMessage: "Password has to be strong" },
+  },
+  password_confirm: {
+    if: (_, { req }) => req.body.password,
+    notEmpty: { errorMessage: "Password Confirmation can't be empty" },
+    custom: {
+      options: passwordConfirmation,
+      errorMessage: "Password and Password Confirmation not equal",
+    },
+  },
 };
