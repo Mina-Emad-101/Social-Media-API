b706af11e21100bcbefcbf3bb8f3b11b7cf561d5
Mina Emad <menamanos@gmail.com>
2024-09-11 01:09:51 +0300

    fixed async error catching

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -5,26 +5,28 @@ import User from "../models/user.js";
 const router = Router();
 
 router.get("/api/users/friends", verifyJWT, async (req, res) => {
-	const friends = req.user.friends;
-	return res.json({ data: friends });
+  const friends = req.user.friends;
+  return res.json({ data: friends });
 });
 
 router.post("/api/users/friends/:id", verifyJWT, async (req, res) => {
-	const user = req.user;
-	const id = req.params.id;
+  const user = req.user;
+  const id = req.params.id;
 
-	if (!id) return res.sendStatus(400);
+  if (!id) return res.sendStatus(400);
 
-	if (user.id === id) return res.sendStatus(403);
+  if (user.id === id) return res.sendStatus(403);
 
-	if (user.friends.includes(id)) return res.sendStatus(403);
+  if (user.friends.includes(id)) return res.sendStatus(403);
 
-	const friend = await User.findById(id);
-	if (!friend) return res.sendStatus(404);
+  const friend = await User.findById(id);
+  if (!friend) return res.sendStatus(404);
 
-	user.friends.push(friend.id);
-	await user.save().catch((err) => res.status(400).json({ error: err }));
-	return res.sendStatus(200);
+  user.friends.push(friend.id);
+  await user.save().then(
+    (_) => res.sendStatus(200),
+    (err) => res.status(400).json({ error: err }),
+  );
 });
 
 export default router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,87 +3,87 @@ import User from "../models/user.js";
 import { verifyJWT } from "../utils/middlewares.js";
 import { checkSchema, validationResult } from "express-validator";
 import {
-	createSchema,
-	patchSchema,
-	putSchema,
+  createSchema,
+  patchSchema,
+  putSchema,
 } from "../validationSchemas/users.js";
 
 const router = Router();
 
 router.get("/api/users", verifyJWT, async (req, res) => {
-	const user = await User.findById(req.user.id);
-	return res.json(user);
+  const user = await User.findById(req.user.id);
+  return res.json(user);
 });
 
 router.post("/api/users", checkSchema(createSchema), async (req, res) => {
-	const result = validationResult(req);
-	if (!result.isEmpty())
-		return res.status(400).json({ validation_errors: result.array() });
+  const result = validationResult(req);
+  if (!result.isEmpty())
+    return res.status(400).json({ validation_errors: result.array() });
 
-	const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-	const user = new User({
-		username: username,
-		email: email,
-		password: password,
-		friends: [],
-	});
+  const user = new User({
+    username: username,
+    email: email,
+    password: password,
+    friends: [],
+  });
 
-	await user.save().then(
-		(_) => res.json(user),
-		(err) => res.status(400).json({ error: err }),
-	);
+  await user.save().then(
+    (_) => res.json(user),
+    (err) => res.status(400).json({ error: err }),
+  );
 });
 
 router.put(
-	"/api/users",
-	checkSchema(putSchema),
-	verifyJWT,
-	async (req, res) => {
-		const result = validationResult(req);
-		if (!result.isEmpty()) return res.json({ validationError: result.array });
+  "/api/users",
+  checkSchema(putSchema),
+  verifyJWT,
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) return res.json({ validationError: result.array });
 
-		const { username, email, password } = req.body;
+    const { username, email, password } = req.body;
 
-		await User.updateOne(
-			{ id: req.user.id },
-			{
-				username: username,
-				email: email,
-				password: password,
-			},
-		);
+    await User.updateOne(
+      { id: req.user.id },
+      {
+        username: username,
+        email: email,
+        password: password,
+      },
+    );
 
-		return res.sendStatus(200);
-	},
+    return res.sendStatus(200);
+  },
 );
 
 router.patch(
-	"/api/users",
-	checkSchema(patchSchema),
-	verifyJWT,
-	async (req, res) => {
-		const result = validationResult(req);
-		if (!result.isEmpty()) return res.json({ validationError: result.array });
+  "/api/users",
+  checkSchema(patchSchema),
+  verifyJWT,
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) return res.json({ validationError: result.array });
 
-		const { username, email, password } = req.body;
+    const { username, email, password } = req.body;
 
-		const user = await User.findById(req.user.id);
+    const user = await User.findById(req.user.id);
 
-		user.username = username ?? user.username;
-		user.email = email ?? user.email;
-		user.password = password ?? user.password;
+    user.username = username ?? user.username;
+    user.email = email ?? user.email;
+    user.password = password ?? user.password;
 
-		await user.save();
+    await user.save();
 
-		return res.sendStatus(200);
-	},
+    return res.sendStatus(200);
+  },
 );
 
 router.delete("/api/users", verifyJWT, async (req, res) => {
-	const user = User.findById(req.user.id);
-	await user.deleteOne();
-	return res.json(user);
+  const user = User.findById(req.user.id);
+  await user.deleteOne();
+  return res.json(user);
 });
 
 export default router;
