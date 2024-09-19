a16189a23efaf4f27bda0cfa333aca1d41834694
Mina Emad <menamanos@gmail.com>
2024-09-19 05:47:54 +0300

    endpoint uri change

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -4,12 +4,12 @@ import User from "../models/user.js";
 
 const router = Router();
 
-router.get("/api/users/friends", verifyJWT, async (req, res) => {
+router.get("/api/friends", verifyJWT, async (req, res) => {
   const friends = req.user.friends;
   return res.json({ data: friends });
 });
 
-router.post("/api/users/friends/:id", verifyJWT, async (req, res) => {
+router.post("/api/friends/:id", verifyJWT, async (req, res) => {
   const user = req.user;
   const id = req.params.id;
 
