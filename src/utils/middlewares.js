7830119d89d190143b3763c02c0362e1e5b9ed59
Mina Emad <menamanos@gmail.com>
2024-09-23 06:01:03 +0300

    bug fix

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -4,22 +4,21 @@ import Post from "../models/post.js";
 import Comment from "../models/comment.js";
 
 export const verifyJWT = (req, res, next) => {
-  const auth_header = req.headers["authorization"];
-  let token = null;
   try {
-    token = auth_header.split(" ")[1];
+    const auth_header = req.headers["authorization"];
+    const token = auth_header.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+      if (err) return res.sendStatus(401);
+
+      const loggedInUser = await User.findById(user.id);
+      req.user = loggedInUser;
+
+      return next();
+    });
   } catch (err) {
     return res.json({ error: "Authorization header must contain Token" });
   }
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
-    if (err) return res.sendStatus(401);
-
-    const loggedInUser = await User.findById(user.id);
-    req.user = loggedInUser;
-
-    return next();
-  });
 };
 
 export const getPostFromID = async (req, res, next) => {
